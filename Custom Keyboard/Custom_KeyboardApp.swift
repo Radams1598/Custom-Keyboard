b428345db92817a9a3fc9b4146e54ad6a163beaf
Radams1598 <42849903+Radams1598@users.noreply.github.com>
2023-12-08 20:47:15 -0600

    Initial Commit

---
+++ Custom Keyboard/Custom_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_KeyboardApp.swift
+//  Custom Keyboard
+//
+//  Created by Raymond Adams on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Custom_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
